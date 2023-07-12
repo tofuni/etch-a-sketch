d33fe7564858133ae5172ae09f6aef0cce704895
Tiffani <112773709+tofuni@users.noreply.github.com>
2023-07-11 23:20:43 -0700

    Create 16x16 grid using flexbox method

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const gridContainer = document.querySelector('.grid-container');
+const containerWidth = gridContainer.clientWidth;
+const containerHeight = gridContainer.clientHeight;
 
+function createGrid(number) {
+    for (let i = 0; i < (number*number); i++) {
+        const div = document.createElement('div');
+        div.style.width = ((containerWidth/number) - 2) + "px";
+        div.style.height = ((containerHeight/number) - 2) + "px";
+        div.classList.add('grid-square');
+        gridContainer.appendChild(div);
+    }
+}
+
+createGrid(16);
