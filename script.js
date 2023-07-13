1285d4bf31bdf71dfb7bac22e15cd1476929eec3
Tiffani <112773709+tofuni@users.noreply.github.com>
2023-07-12 19:53:18 -0700

    Create 16x16 grid using CSS grid layout

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+// Create 16x16 grid
 const gridContainer = document.querySelector('.grid-container');
-const containerWidth = gridContainer.clientWidth;
-const containerHeight = gridContainer.clientHeight;
 
 function createGrid(number) {
     for (let i = 0; i < (number*number); i++) {
+        gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
         const div = document.createElement('div');
-        div.style.width = ((containerWidth/number) - 2) + "px";
-        div.style.height = ((containerHeight/number) - 2) + "px";
         div.classList.add('grid-square');
         gridContainer.appendChild(div);
     }
